7427aabaaa82f132ff1d1266fee0010c9250b98f
Guilhem Raux <g.raux@icloud.com>
2024-02-06 16:08:22 +0100

    refactored map cells creation

--- src/game_world/mod.rs
+++ src/game_world/mod.rs
@@ -27,10 +27,12 @@ pub struct MapCellBundle {
 
 fn setup_game_map(mut commands: Commands, atlas: Res<Sprites>) {
     println!("Setting up game map");
-    for row in -(GRID_HEIGHT as i32 + 1) / 2..(GRID_HEIGHT as i32 + 1) / 2 {
-        for col in -(GRID_WIDTH as i32) / 2..GRID_WIDTH as i32 / 2 + 1 {
+    for row in 0..GRID_HEIGHT {
+        for col in 0..GRID_WIDTH {
             let mut sprite = TextureAtlasSprite::new(3);
-            if row == 0 && col == 0 {
+            if row as i32 - (GRID_HEIGHT / 2) as i32 == 0
+                && col as i32 - (GRID_WIDTH / 2) as i32 == 0
+            {
                 sprite.color = Color::rgb(0.0, 0.8, 0.0);
             } else {
                 sprite.color = Color::rgb(0.8, 0.0, 0.0);
@@ -42,8 +44,8 @@ fn setup_game_map(mut commands: Commands, atlas: Res<Sprites>) {
                     sprite,
                     texture_atlas: atlas.0.clone(),
                     transform: Transform::from_xyz(
-                        GRID_SIZE as f32 * col as f32,
-                        GRID_SIZE as f32 * row as f32,
+                        GRID_SIZE as f32 * (col as f32 - (GRID_WIDTH / 2) as f32),
+                        GRID_SIZE as f32 * (row as f32 - (GRID_HEIGHT / 2) as f32),
                         0.0,
                     ),
                     ..default()
