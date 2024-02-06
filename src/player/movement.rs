d04579dde7ee5d4c47e408dd8a82a877a62469a3
Guilhem Raux <g.raux@icloud.com>
2024-02-06 19:23:41 +0100

    added diagonal keys for player movement

--- src/player/movement.rs
+++ src/player/movement.rs
@@ -56,6 +56,22 @@ pub fn player_movement(keys: Res<Input<KeyCode>>, mut query: Query<&mut Movement
     if keys.pressed(KeyCode::A) {
         dir.0 += -1.0;
     }
+    if keys.pressed(KeyCode::Q) {
+        dir.0 = -1.0;
+        dir.1 = 1.0;
+    }
+    if keys.pressed(KeyCode::E) {
+        dir.0 = 1.0;
+        dir.1 = 1.0;
+    }
+    if keys.pressed(KeyCode::C) {
+        dir.0 = 1.0;
+        dir.1 = -1.0;
+    }
+    if keys.pressed(KeyCode::Z) {
+        dir.0 = -1.0;
+        dir.1 = -1.0;
+    }
     let mut mov = query.single_mut();
     mov.directions.0 = dir.0;
     mov.directions.1 = dir.1;
