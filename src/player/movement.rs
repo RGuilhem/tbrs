6e68524c0c40bb1b7bc0e6bdfc5a3a6a9c1ca264
Guilhem Raux <g.raux@icloud.com>
2024-02-06 03:41:27 +0100

    opposite inputs now cancel eachother

--- src/player/movement.rs
+++ src/player/movement.rs
@@ -19,16 +19,16 @@ impl Default for Movement {
 pub fn player_movement(keys: Res<Input<KeyCode>>, mut query: Query<&mut Movement, With<Player>>) {
     let mut dir = (0.0, 0.0);
     if keys.pressed(KeyCode::W) {
-        dir.1 = 1.0;
+        dir.1 += 1.0;
     }
     if keys.pressed(KeyCode::S) {
-        dir.1 = -1.0;
+        dir.1 += -1.0;
     }
     if keys.pressed(KeyCode::D) {
-        dir.0 = 1.0;
+        dir.0 += 1.0;
     }
     if keys.pressed(KeyCode::A) {
-        dir.0 = -1.0;
+        dir.0 += -1.0;
     }
     let mut mov = query.single_mut();
     mov.directions.0 = dir.0;
