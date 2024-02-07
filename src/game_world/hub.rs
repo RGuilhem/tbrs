c9637e89beb21ee90fc5cf9f7dad38ee5687bb6a
Guilhem Raux <g.raux@icloud.com>
2024-02-07 19:59:23 +0100

    added a function to get sprite transform from "grid" pos

--- src/game_world/hub.rs
+++ src/game_world/hub.rs
@@ -1,5 +1,15 @@
+use crate::sprites::sprite_index;
+use crate::sprites::transform_from_grid;
 use crate::sprites::Sprites;
 use bevy::prelude::*;
 
 pub fn setup_hub(mut commands: Commands, atlas: Res<Sprites>) {
+    let mut sprite = TextureAtlasSprite::new(sprite_index(0, 1));
+    sprite.color = Color::rgb(0.1, 0.75, 0.1);
+    commands.spawn(SpriteSheetBundle {
+        sprite,
+        texture_atlas: atlas.0.clone(),
+        transform: transform_from_grid(1, 1, 0),
+        ..default()
+    });
 }
--- src/sprites.rs
+++ src/sprites.rs
@@ -1,3 +1,4 @@
+use crate::GRID_SIZE;
 use bevy::prelude::*;
 
 #[derive(Resource)]
@@ -8,11 +9,25 @@ impl FromWorld for Sprites {
         let asset_server = world.get_resource_mut::<AssetServer>().unwrap();
         let texture_handle = asset_server.load("tilesheet.png");
 
-        let atlas =
-            TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 49, 22, None, None);
+        let atlas = TextureAtlas::from_grid(
+            texture_handle,
+            Vec2::new(GRID_SIZE as f32, GRID_SIZE as f32),
+            49,
+            22,
+            None,
+            None,
+        );
         let mut texture_atlases = world.get_resource_mut::<Assets<TextureAtlas>>().unwrap();
         let atlas_handle = texture_atlases.add(atlas);
 
         Self(atlas_handle)
     }
+}
+
+pub fn transform_from_grid(x: i32, y: i32, z: i32) -> Transform {
+    Transform::from_xyz(GRID_SIZE as f32 * x as f32, GRID_SIZE as f32 * y as f32, z as f32)
+}
+
+pub fn sprite_index(col: u32, row: u32) -> usize {
+    (row * 49 + col) as usize
 }
